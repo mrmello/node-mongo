3969e13537bbe1bf0f43e6d5140e1f56d1ef84e0
Marcos Rogério de Mello <marcos.mello@pampa.compasso>
2018-01-25 14:31:15 -0200

    returning books as an example

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose');
-const keys = require('./config/keys');
-require('./models/Book');
+//const mongoose = require('mongoose');
+//const keys = require('./config/keys');
+//require('./models/Book');
 const app = express()
 
-mongoose.connect(keys.mongoURI)
-  .catch(err => {console.log('cannot connect ' + err)});
-
-const Book = mongoose.model('books');
+// mongoose.connect(keys.mongoURI)
+//   .catch(err => {console.log('cannot connect ' + err)});
+//
+// const Book = mongoose.model('books');
 
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.get('/', function (req, res) {
+app.get('/books', function (req, res) {
 
-  new Book({
-    id: '001',
-    title:  'First Book Of Shelf',
-    author: 'Marcos Mello',
-    pages:  '289'
-  }).save()
-    .catch(err => {
-      res.send(err);
-    })
-    .then(book => {
-      res.send(book);
-    })
-
-
-  res.send('eeitaa')
+  const json = [
+    {
+      id: '001',
+      title:  'Getting started with react',
+      author: 'Marcos Mello',
+      pages:  '289'
+    },
+    {
+      id: '002',
+      title:  'Ruby: the gem',
+      author: 'Dyan Carra',
+      pages:  '104'
+    },
+    {
+      id: '003',
+      title:  'The Android master',
+      author: 'Alvondi J.',
+      pages:  '320'
+    }];
+  res.send(json);
 })
 
 const PORT = process.env.PORT || 3000;
